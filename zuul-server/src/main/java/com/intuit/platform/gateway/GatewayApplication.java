b7b72c70bfff9848249ff392c9f95e9c8218a758
Marcello de Sales <Marcello_deSales@intuit.com>
2017-04-26 14:46:08 -0700

    Initial implementation of the Zuul API Gateway server

---
+++ zuul-server/src/main/java/com/intuit/platform/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.intuit.platform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+}
